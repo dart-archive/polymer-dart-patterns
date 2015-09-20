d42e7a202b27e434487579438c30b3f51c55386f
zoechi <gzoechi@gmail.com>
2015-09-20 17:42:05 +0200

    - silence the analyzer about unused import

--- web/styling_elements/applying_styles_by_piercing_shadow_dom_boundaries/my_element.dart
+++ web/styling_elements/applying_styles_by_piercing_shadow_dom_boundaries/my_element.dart
@@ -3,8 +3,9 @@ library my_element;
 
 import 'package:web_components/web_components.dart' show HtmlImport;
 import 'package:polymer/polymer.dart';
-import 'child_element.dart' as child_element;
+import 'child_element.dart';
 
+/// Silence analyzer [ChildElement]
 @PolymerRegister('my-element')
 class MyElement extends PolymerElement {
   MyElement.created() : super.created();
