0c9367472f4be3e7cdf66711f3d5e76cd5606cc8
zoechi <gzoechi@gmail.com>
2015-10-21 14:03:48 +0200

    simplify example by using template repeats filter and observe attributes

--- web/forms/selecting_many_items_using_checkboxes/my_element.dart
+++ web/forms/selecting_many_items_using_checkboxes/my_element.dart
@@ -20,18 +20,6 @@ class MyElement extends PolymerElement {
     new Item('blue', false)
   ];
 
-  @property List<String> selectedColors = [];
-
-  @Observe('items.*')
-  void updateSelectedColors(Map record, [_]) {
-    String path = record['path'];
-    if (path == 'items' || path.endsWith('.checked')) {
-      set(
-          'selectedColors',
-          items
-              .where((item) => item.checked)
-              .map((item) => item.color)
-              .toList());
-    }
-  }
+  @reflectable
+  bool filterSelected(Item item) => item.checked;
 }
